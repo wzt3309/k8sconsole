668ea0ea0bd30d60f1ad6c974ac2289cc9937c43
wzt3309 <wzt3309@gmail.com>
2018-10-17 17:04:03 +0800

    Karma configuration for the frontend app

--- build/karma.conf.js
+++ build/karma.conf.js
@@ -1,33 +1,79 @@
-import config from './config';
-import gulp from 'gulp';
-import karma from 'karma';
+import path from 'path'
+import wiredep from 'wiredep';
+
+import config from './config'
 
 /**
- *
- * @param {boolean} singleRun
- * @param {function(?Error=)} doneFn
+ * Returns an array of files required by Karma to run the tests.
+ * @returns {!Array<string>}
  */
-function runUnitTests(singleRun, doneFn) {
-    let localConfig = {
-        configFile: config.paths.karmaConf,
-        singleRun: singleRun,
-        autoWatch: !singleRun
+function getFileList() {
+    // All app dependencies are required for tests. Include them.
+    let wiredepOptions = {
+        dependencies: true,
+        devDependencies: true
     };
 
-    let server = new karma.Server(localConfig, function (failCount) {
-        doneFn(failCount ? new Error("Failed" + failCount + " tests.") : undefined);
-    });
-    server.start();
+    return wiredep(wiredepOptions).js
+        .concat([
+            path.join(config.paths.frontendTest, '**/*.js'),
+            path.join(config.paths.frontendSrc, '**/*.js'),
+            path.join(config.paths.frontendSrc, '**/*.html')
+        ]);
 }
 
-// Run once all unit tests of the application
-gulp.task('test', function (doneFn) {
-    runUnitTests(true, doneFn);
-});
+/**
+ * Exported default function which sets Karma configuration. Required by the framework.
+ *
+ * @param {!Object} conf
+ */
+export default function (conf) {
+    let configuration = {
+        basePath: config.paths.base,
 
-// Runs all unit tests of the application. Watches for changes in the source files to rerun
-// the tests.
+        files: getFileList(),
 
-gulp.task('test:watch', function (doneFn) {
-    runUnitTests(false, doneFn);
-});
+        frameworks: ['jasmine', 'browserify'],
+
+        browsers: ['Chrome'],
+
+        reporters: ['progress'],
+
+        preprocessors: {}, // This field is filled with values later.
+
+        plugins: [
+            'karma-chrome-launcher',
+            'karma-jasmine',
+            'karma-ng-html2js-preprocessor',
+            'karma-sourcemap-loader',
+            'karma-browserify'
+        ],
+
+        // karma-browserify plugin config.
+        browserify: {
+            // Add source maps to outpus bundles.
+            debug: true,
+            // Make 'import ...' statements relative to the following paths.
+            paths: [config.paths.frontendSrc, config.paths.frontendTest],
+            transform: [
+                // Transform ES6 code into ES5 so that browsers can digest it.
+                'babelify'
+            ]
+        },
+
+        // karma-ng-html2js-preprocessor plugin config.
+        ngHtml2JsPreprocessor: {
+            stripPrefix: config.paths.frontendSrc + '/',
+            moduleName: config.frontend.moduleName
+        }
+    };
+
+    // Convert all JS code written ES6 with modules to ES5 bundles that browsers can digest.
+    configuration.preprocessors[path.join(config.paths.frontendTest, '**/*.js')] = ['browserify'];
+    configuration.preprocessors[path.join(config.paths.frontendSrc, '**/*.js')] = ['browserify'];
+
+    // Convert HTML templates into JS files that serve code through $templateCache.
+    configuration.preprocessors[path.join(config.paths.frontendSrc, '**/*.html')] = ['ng-html2js'];
+
+    conf.set(configuration);
+}
