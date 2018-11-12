b05d8a17aff1e5d477a457b2c12637398276bb45
wzt3309 <wzt3309@gmail.com>
2018-11-12 19:55:11 +0800

    Add namespace query and resource channel
    The namespace query is used to query the resource scoped by namespace,
    and resource channel is the go channel to get k8s resource and error
    happened during the 'get' process

--- src/app/backend/api/types.go
+++ src/app/backend/api/types.go
@@ -1,5 +1,69 @@
 package api
 
+import (
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
+)
+
+// CsrfToken is used to secure requests from CSRF attacks
+type CsrfToken struct {
+	// Token generated on request for validation
+	Token string `json:"token"`
+}
+
+// ObjectMeta is metadata about an instance of resource
+type ObjectMeta struct {
+	// Object name and the name is unique within a namespace.
+	Name 								string 							`json:"name,omitempty"`
+
+	// Any empty namespace equivalent to the 'default' namespace.
+	// Not all objects are required to be scoped to a namespace - the value of this field
+	// for those objects will be empty
+	Namespace 					string 							`json:"namespace,omitempty"`
+
+	// Labels are k-v pairs that may be scope and select individual resources.
+	Labels 							map[string]string		`json:"labels,omitempty"`
+
+	// Annotations are unstructured key value data stored with a resource that be set by external tooling.
+	Annotations 				map[string]string 	`json:"annotations,omitempty"`
+
+	// CreationTimestamp is a timestamp representing the apiserver time when this object
+	// was created.
+	CreationTimestamp		metaV1.Time					`json:"creationTimestamp,omitempty"`
+}
+
+// TypeMeta describes the type of an object in response and request
+type TypeMeta struct {
+	// kind of an object
+	Kind	ResourceKind	`json:"kind,omitempty"`
+}
+
+// ListMeta describes list of objects.
+type ListMeta struct {
+	TotalItems int `json:"totalItems"`
+}
+
+// NewObjectMeta creates a new instance of ObjectMate struct based on k8s object meta.
+func NewObjectMeta(k8sObjectMeta metaV1.ObjectMeta) ObjectMeta {
+	return ObjectMeta{
+		Name: k8sObjectMeta.Name,
+		Namespace: k8sObjectMeta.Namespace,
+		Labels: k8sObjectMeta.Labels,
+		Annotations: k8sObjectMeta.Annotations,
+		CreationTimestamp: k8sObjectMeta.CreationTimestamp,
+	}
+}
+
+// NewTypeMeta creates new type meta for the resource kind
+func NewTypeMeta(kind ResourceKind) TypeMeta {
+	return TypeMeta{
+		Kind: kind,
+	}
+}
+
+type ResourceKind string
+
 // List of all resource kinds
 // TODO(wzt3309) Need to add more resource kinds
 const (
@@ -45,4 +109,10 @@ var KindToAPIMapping = map[string]struct {
 	ResourceKindReplicaSet: {"replicasets", ClientTypeExtensionClient, true},
 	ResourceKindSecret:     {"secrets", ClientTypeDefault, true},
 	ResourceKindService:    {"services", ClientTypeDefault, true},
+}
+
+// ListEverything is a list options used to list all resource without any filtering.
+var ListEverything = metaV1.ListOptions{
+	LabelSelector: labels.Everything().String(),
+	FieldSelector: fields.Everything().String(),
 }
---
+++ src/app/backend/resource/common/resourcechannels.go
@@ -0,0 +1,81 @@
+package common
+
+import (
+	"github.com/wzt3309/k8sconsole/src/app/backend/api"
+	"k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	client "k8s.io/client-go/kubernetes"
+)
+
+type ResourceChannels struct {
+	// List and error channels to Pods.
+	PodList PodListChannel
+
+	// List and error channels to Namespace.
+	NamespaceList NamespaceListChannel
+}
+
+// PodListChannel is a list and error channels to nodes
+type PodListChannel struct {
+	List chan *v1.PodList
+	Error chan error
+}
+
+// GetPodListChannel returns a pair of channels to a Pod list and errors that both must be
+// read numReads times.
+func GetPodListChannel(client client.Interface,
+	nsQuery *NamespaceQuery, numReads int) PodListChannel {
+	return GetPodListChannelWithOptions(client, nsQuery, api.ListEverything, numReads)
+}
+
+// GetPodListChannelWithOptions is the GetPodListChannel plus list options.
+func GetPodListChannelWithOptions(client client.Interface, nsQuery *NamespaceQuery,
+	options metaV1.ListOptions, numReads int) PodListChannel {
+
+	channel := PodListChannel{
+		List: make(chan *v1.PodList, numReads),
+		Error: make(chan error, numReads),
+	}
+
+	go func() {
+		list, err := client.CoreV1().Pods(nsQuery.ToRequestParam()).List(options)
+		var filterItems []v1.Pod
+		for _, item := range list.Items {
+			if nsQuery.Matches(item.ObjectMeta.Namespace) {
+				filterItems = append(filterItems, item)
+			}
+		}
+		list.Items = filterItems
+		for i := 0; i < numReads; i++ {
+			channel.List <- list
+			channel.Error <- err
+		}
+	}()
+
+	return channel
+}
+
+// NamespaceListChannel is a list and error channels to Namespaces.
+type NamespaceListChannel struct {
+	List chan *v1.NamespaceList
+	Error chan error
+}
+
+// GetNamespaceListChannel returns a pair of channels to a Namespace list and errors that
+// must be read numReads times.
+func GetNamespaceListChannel(client client.Interface, numReads int) NamespaceListChannel {
+	channel := NamespaceListChannel{
+		List: make(chan *v1.NamespaceList, numReads),
+		Error: make(chan error, numReads),
+	}
+
+	go func() {
+		list, err := client.CoreV1().Namespaces().List(api.ListEverything)
+		for i := 0; i < numReads; i++ {
+			channel.List <- list
+			channel.Error <- err
+		}
+	}()
+
+	return channel
+}
